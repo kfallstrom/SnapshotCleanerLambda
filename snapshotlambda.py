85e76d7d608d7ac789683822d5c30601954f4c5e
Karl Fallstrom <karl@xykonconsulting.com>
2019-01-19 22:57:21 -0800

    Update snapshotlambda.py
    add some logic for finding mondays

--- snapshotlambda.py
+++ snapshotlambda.py
@@ -3,6 +3,13 @@
     client = boto3.client('ec2', 'us-east-1'')
     ec2 = boto3.resource('ec2', 'us-east-1')
     today_date = time.strptime(today_time, '%m-%d-%Y')
+    thisyearandnextyearmondays = []
+   #365 + 365 = 730 days
+    for x in range(730):
+     #if date is a monday, add to list
+     ordinalvalue = date.today().replace(month=1,day=1).toordinal()+x
+     if date.fromordinal(ordinalvalue).weekday()==0:
+      thisyearandnextyearmondays.append(date.fromordinal(ordinalvalue))
     
     
 def lambda_handler(event, context):                      
