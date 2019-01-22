ee8cae3007e2fc6ee01f9876e3d73928b59900fa
Karl Fallstrom <karl@xykonconsulting.com>
2019-01-22 06:38:33 -0800

    Update snapshotlambda.py

--- snapshotlambda.py
+++ snapshotlambda.py
@@ -1,20 +1,34 @@
 import boto3
-import datetime
+from datetime import date
 client = boto3.client('ec2', 'us-east-1'')
 ec2 = boto3.resource('ec2', 'us-east-1')
 thisyearandnextyearmondays = []
 firstmondays = []
+earliestmonday = None
 #365 + 365 = 730 days
 for x in range(730):
  #if date is a monday, add to list
  ordinalvalue = date.today().replace(month=1,day=1).toordinal()+x
  datevalue = date.fromordinal(ordinalvalue)
+ #only Mondays
  if datevalue.weekday()==0:
-  lastvalue = thisyearandnextyearmondays[-1]
-#  if(lastvalue.month = datevalue.month and lastvalue.year = datevalue.year and lastvalue.day<datevalue.day):
-#   print 
+  if earliestmonday is None:
+   earliestmonday = datevalue
+#keep earliest monday for the month
   thisyearandnextyearmondays.append(datevalue)
-    
+  if earliestmonday.month == datevalue.month and earliestmonday.year == datevalue.year and datevalue.day<earliestmonday.day:
+   earliestmonday=datevalue
+  elif earliestmonday.month < datevalue.month and earliestmonday.year == datevalue.year:
+   firstmondays.append(earliestmonday)
+   earliestmonday = None
+  else:
+   continue
+for x in firstmondays:
+ print(x)
+print "all mondays"
+for x in thisyearandnextyearmondays:
+ print(x)
+
     
 def lambda_handler(event, context):                      
                           
